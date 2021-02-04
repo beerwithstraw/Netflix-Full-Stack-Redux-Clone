e51553c83228973b23e44492e38ca2e88882ec00
beerwithstraw <agarwalp@tcd.ie>
2021-02-04 00:48:07 +0000

    Firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA1HP06mazjixTlShhypXYSDSltVVRPAlI",
+  authDomain: "netflix-full-stack-clone.firebaseapp.com",
+  projectId: "netflix-full-stack-clone",
+  storageBucket: "netflix-full-stack-clone.appspot.com",
+  messagingSenderId: "605440231974",
+  appId: "1:605440231974:web:ee70690f91fe42148374e7"
+};
