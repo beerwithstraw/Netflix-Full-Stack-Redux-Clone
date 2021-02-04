c94d3cfcd1d03266e9877e5230376b76cdc66744
beerwithstraw <agarwalp@tcd.ie>
2021-02-04 20:20:21 +0000

    HomeScreen Relocate and update Locations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
-        <HomeScreen />
+
+        <Router>
+          <Switch>
+            {/* <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route> */}
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
