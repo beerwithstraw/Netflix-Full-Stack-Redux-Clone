9ebb35235a3e8f287328d8e52ea1c224139e5735
beerwithstraw <agarwalp@tcd.ie>
2021-02-04 01:41:29 +0000

    Nav Bar Style & Animation

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import "./HomeScreen.css"
+import Nav from './Nav'
 
 function HomeScreen() {
     return (
-        <div>
-            
+        <div className="homeScreen">
+            <Nav />
         </div>
     )
 }
