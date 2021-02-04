e495a4a0b38de848d082e84eac8fc95ade715b5a
beerwithstraw <agarwalp@tcd.ie>
2021-02-04 00:59:25 +0000

    HomeScreen Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomeScreen from './HomeScreen';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Let's Build Netflix Clone</h1>
+    <div className="app">
+        <HomeScreen />
     </div>
   );
 }
