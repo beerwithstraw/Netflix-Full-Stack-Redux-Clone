e2c47a22d9f8a18e0944b48bee85d69a0064a818
beerwithstraw <agarwalp@tcd.ie>
2021-02-04 03:40:08 +0000

    Rows with API Movie Cards

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,12 +2,25 @@ import React from 'react'
 import Banner from './Banner'
 import "./HomeScreen.css"
 import Nav from './Nav'
+import requests from './Requests'
+import Row from './Row'
 
 function HomeScreen() {
     return (
         <div className="homeScreen">
             <Nav />
             <Banner />
+
+            <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending}  />
+            {/* <Row title="Top rated" fetchUrl={requests.fetchTopRated}  /> */}
+            <Row title="ActionMovies" fetchUrl={requests.fetchActionMovies}  />
+            <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}  />
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}  />
+            <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}  />
+            <Row title="Documantaries" fetchUrl={requests.fetchDocumentaries}  />
+
+
         </div>
     )
 }
