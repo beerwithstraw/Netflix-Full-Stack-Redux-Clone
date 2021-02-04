7d37ca0b34311d40e6c224ba32861d37543845b2
beerwithstraw <agarwalp@tcd.ie>
2021-02-04 02:34:28 +0000

    Movie Background Banner from API

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from './axios'
+import requests from './Requests'
 import "./Banner.css"
 
 function Banner() {
+
+    const [movie, setMovie] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            )
+            return request;
+        }
+
+        fetchData()
+    }, [])
 
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n-1) + '...' : string;
@@ -10,7 +28,7 @@ function Banner() {
     return (
         <header className="banner" style={{
             backgroundSize: "cover",
-            backgroundImage: `url("https://xeraone.com/wp-content/uploads/Daredevil-Banner-1024x474.png")`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition: "center center",
         }}>
             <div className="banner__contents">
