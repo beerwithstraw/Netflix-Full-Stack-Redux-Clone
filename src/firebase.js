8ae89f021c02c5ed4c5fdb64f880824a0df32364
beerwithstraw <agarwalp@tcd.ie>
2021-02-05 05:05:37 +0000

    firebase exports db and auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: "AIzaSyA1HP06mazjixTlShhypXYSDSltVVRPAlI",
   authDomain: "netflix-full-stack-clone.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "605440231974",
   appId: "1:605440231974:web:ee70690f91fe42148374e7"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+export { auth }
+export default db;
