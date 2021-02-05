bb0938f1123b06249c63554c782d4459fb30e057
beerwithstraw <agarwalp@tcd.ie>
2021-02-05 05:06:59 +0000

    Route functionality on icons

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom';
 import "./Nav.css"
 function Nav() {
 
     const [show, handleShow] = useState(false);
+    const history = useHistory()
 
     const transitionNavBar = () => {
         if(window.scrollY > 300) {
@@ -20,9 +22,10 @@ function Nav() {
     return (
         <div className={`nav  ${show && 'nav__black'}`}>
             <div className="nav__contents">
-                <img className="nav__logo"
+                <img className="nav__logo" onClick={() => history.push("/")}
                     src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="Netflix Logo"/>
-                <img className="nav__avatar" 
+
+                <img className="nav__avatar" onClick={() => history.push("/profile")}
                     src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Netflix Avatar"/>
             </div>
 
